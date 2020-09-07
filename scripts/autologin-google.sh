1df5ba5596fdb2b4d7b1e495612610cc492427d8
kopijahe <32376719+kopijahe@users.noreply.github.com>
2020-09-07 09:19:13 +0700

    Autologin: google: drop wget dependency
    4MB ROM users rejoice

--- scripts/autologin-google.sh
+++ scripts/autologin-google.sh
@@ -3,11 +3,6 @@
 # Script untuk login otomatis di jaringan wifi.id
 # Oleh: KopiJahe (https://github.com/kopijahe)
 #
-# Varian ini membutuhkan paket wget terpisah
-# agar bisa mendeteksi redirect ke halaman login.
-# Jangan lupa untuk memasang paketnya dengan perintah:
-# opkg update && opkg install wget
-#
 # Tentukan variabel loginwifi dari berkas /etc/login_file.txt
 # Jangan lupa untuk berikan izin eksekusi berkas dengan perintah:
 # chmod +x /etc/login_file.txt
@@ -19,10 +14,10 @@ while [ true ]; do
 # Cek apakah berkas ini tersedia atau tidak
 # dengan batasan redirect 1 kali:
 # http://connectivitycheck.google.com/generate_204
-# dan simpan hasilnya di /dev/null supaya tidak memakan tempat penyimpanan
-/usr/bin/wget -q --spider --max-redirect=1 http://connectivitycheck.gstatic.com/generate_204 > /dev/null
-# Jika tidak terdapat error (kode 0), maka:
-if [[ $? -eq 0 ]]; then
+# dan simpan hasilnya di stdout supaya tidak memakan tempat penyimpanan
+status=$(curl --silent --max-redirs 1 -LI "http://connectivitycheck.gstatic.com/generate_204" | grep -o "204")
+# Jika respon dari server adalah "HTTP/1.1 204 No Content", maka:
+if [[ "$status" = "204" ]]; then
 # Beritahu pengguna bahwa sudah terkoneksi dengan Internet
 # Dan simpan hasilnya di /tmp/internet.status untuk pengecekan
 echo "Sudah terkoneksi dengan Internet" | tee /tmp/internet.status
