6eea4dd3d8bb2c3c6ead8638c45e1ec8b57249f6
kopijahe <32376719+kopijahe@users.noreply.github.com>
2020-09-27 09:24:11 +0700

    Autologin: re-add WMS Venue
    Regular script won't work on venue WMS because they add 4 random characters after the username for each login.
    
    Create separate document on this because there are additional steps too.

---
+++ scripts/autologin-wms-firefox.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# Script untuk login otomatis di jaringan wifi.id
+# Oleh: KopiJahe (https://github.com/kopijahe)
+#
+# Tentukan lokasi berkas login_file.txt
+filelogintxt=/etc/login_file.txt
+# Tentukan lokasi berkas sementara
+loginwms=/tmp/login_file.txt
+
+# Selama script berjalan, lakukan hal ini:
+while [ true ]; do
+# Tentukan variabel status dari hasil unduhan berkas
+# dari: http://detectportal.firefox.com/success.txt
+# dan simpan hasilnya di stdout
+status=$(curl --silent --max-redirs 1 --connect-timeout 10  "http://detectportal.firefox.com/success.txt")
+# Jika variabel status hasil unduhan tadi sama dengan "success", maka
+if [[ "$status" = "success" ]]; then
+# Beritahu pengguna bahwa sudah terkoneksi dengan Internet
+# Dan simpan hasilnya di /tmp/internet.status untuk pengecekan
+echo "Sudah terkoneksi dengan Internet" | tee /tmp/internet.status
+# Jika hasilnya tidak sama, berarti tidak terkoneksi dengan Internet, maka:
+else
+# Gandakan berkas login_file.txt ke lokasi berkas sementara
+cp "$filelogintxt" "$loginwms"
+# Buat variabel randomid yang terdiri dari 4 karakter angka dan huruf acak
+# Hal ini diperlukan karena sistem login WMS menambahkan 4 karakter acak
+# Setelah username pengguna setiap kali login
+randomid=$(head -4 /dev/urandom | tr -dc "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" | head -c4)
+# Masukkan hasil randomid ke berkas sementara
+sed "s/kopijahe/$randomid/g" $loginwms
+# Beri izin eksekusi berkas sementara
+chmod +x $loginwms
+# Catat tanggal dan jam login terakhir,
+echo "Percobaan login terakhir:" | tee /tmp/last.login
+date | tee -a /tmp/last.login
+# Catat pula status percobaan login terakhir
+echo "Status percobaan login terakhir:" | tee -a  /tmp/last.login
+# Dan lakukan login, serta catat semua hasilnya di berkas /tmp/last.login untuk pengecekan
+$loginwms | tee -a /tmp/internet.status /tmp/last.login | logger
+fi
+# Istirahat selama 10 detik sebelum melakukan pengecekan lagi
+sleep 10
+# Selesai
+done
